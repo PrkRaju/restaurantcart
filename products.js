e9039cb3fd38acb50d51b07edbbda144db75d1a0
PrkRaju <91650716+PrkRaju@users.noreply.github.com>
2022-01-17 22:41:19 +0530

    Cart for Restaurant

---
+++ products.js
@@ -0,0 +1,111 @@
+
+var products = {
+  1: {
+    name : "Momos",
+    desc : "Chinese momos",
+    img : "momos.jpg",
+    price : 80
+  },
+  2: {
+    name : "Chilli Chicken",
+    desc : "Spicy Chilli Chicken",
+    img : "Chilli chicken.png",
+    price : 120
+  },
+  3: {
+    name : "Fried Fish",
+    desc : "Apollo Fish",
+    img : "chinesefish.jpg",
+    price : 150
+  },
+  4: {
+    name : "Steamed Shrimp",
+    desc : "Well Steamed Shrimp",
+    img : "chinesechrimp.jpg",
+    price : 180
+  },
+  5: {
+    name : "Choclate Brownie",
+    desc : "Hot melting brownie",
+    img : "chocobrownie.jpg",
+    price : 50
+  },
+  6: {
+    name : "Choco Pastry",
+    desc : "Cold Choclate Pastry",
+    img : "chocopastry.jpg",
+    price : 75
+  },
+  7: {
+    name : "Choco Cup-Cake",
+    desc : "Choclate Cup Cake",
+    img : "chococupcake.jpg",
+    price : 35
+  },
+  8: {
+    name : "Choclate Milk",
+    desc : "Hot Choclate Milk",
+    img : "chocomason.jpg",
+    price : 60
+  },
+  9: {
+    name : "Oreo Milk-Shake",
+    desc : "Fresh Oreo Mlikshake",
+    img : "oreo.jpg",
+    price : 130
+  },
+  10: {
+    name : "French Fries",
+    desc : "Potato Fries",
+    img : "frenchfries.jpg",
+    price : 80
+  },
+  11: {
+    name : "Ice Cream",
+    desc : "Cold Ice Cream",
+    img : "ice cream.png",
+    price : 70
+  },
+  12: {
+    name : "Chicken Burger",
+    desc : "Double Patty Burger",
+    img : "beef burger.jpg",
+    price : 170
+  },
+  13: {
+    name : "Indian Biryani",
+    desc : "Hyderabadi Dum Biryani",
+    img : "indianbiryani.jpg",
+    price : 170
+  },
+  14: {
+    name : "Butter Chicken",
+    desc : "Indian Butter Chicken",
+    img : "indianbutter.jpg",
+    price : 160
+  },
+  15: {
+    name : "Butter Paneer",
+    desc : "Indian Paneer",
+    img : "indianpaneer.jpg",
+    price : 80
+  },
+  16: {
+    name : "Keema Matar",
+    desc : "Indian Keema matar",
+    img : "indiankeeemamatar.jpg",
+    price : 125
+  },
+  17: {
+    name : "Pasta",
+    desc : "Italian pasta",
+    img : "italianpasta.jpg",
+    price : 100
+  },
+  18: {
+    name : "Pizza",
+    desc : "Italian Pizza",
+    img : "italianpizza.jpeg",
+    price : 200
+  },
+};
